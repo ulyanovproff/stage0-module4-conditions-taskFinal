b892e2a6eb82568da04a4a5ea1874081bc2d3322
pavel.ulianov <ulyanovproff@gmail.com>
2024-01-08 20:48:08 +0500

    This is final task if-else

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+
+
+        if (divider > 0) {
+            int count = dividend / divider;
+            int qty = count * divider;
+            if (dividend == qty) {
+                System.out.println("can be divided completely");
+            } else {
+                System.out.println("cannot be divided completely");
+            }
+
+        } else {
+            System.out.println("division by zero");
+        }
     }
+
 }
+
+
+
+
